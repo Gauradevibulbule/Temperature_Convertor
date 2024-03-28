45683ccb49b1f144cb2b19460def0c1190afc2f8
Gauradevibulbule <162315945+Gauradevibulbule@users.noreply.github.com>
2024-03-28 11:41:51 +0530

    settings.py

--- tempconvert/tempconvert/settings.py
+++ tempconvert/tempconvert/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,4 @@ STATICFILES_DIRS = ['static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
